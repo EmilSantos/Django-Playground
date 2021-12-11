9be515d1f307b8132293c89fbc8c8f9296262ab1
EmilSantos <Santos.N.Emil@gmail.com>
2021-12-11 15:37:46 -0800

    Context & Login play

--- DjangoPlayground/blog/urls.py
+++ DjangoPlayground/blog/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from .views import post_model_list_view
+from .views import (
+    post_model_list_view,
+    post_model_detail_view
+    )
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     url(r'^$', post_model_list_view, name='list'),
+    url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
     #path('redirect/', redirect_somewhere, name='home'),
 ]
--- DjangoPlayground/blog/views.py
+++ DjangoPlayground/blog/views.py
@@ -1,18 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import PostModel
 
+def post_model_detail_view(request, id=None):
+  print(id)
+  obj = get_object_or_404(PostModel, id=id)
+  context = {
+    "object": obj,
+  }
+  template = "blog/detail-view.html"
+  return render(request, template, context)
+
+#@login_required(login_url='/login/')
 def post_model_list_view(request):
+  print(request.user)
   qs = PostModel.objects.all()
-  print(qs)
   template = "blog/list-view.html"
   context = {
     "object_list": qs
-    #"some_dict": {"abc": 123},
-    #"num": 123,
-    #"array_list": [123, 423],
-    #"boolean_value": True,
   }
+  if request.user.is_authenticated:
+    template = "blog/list-view.html"
+  else:
+    template = "blog/list-view-public.html"
   return render(request, template, context)

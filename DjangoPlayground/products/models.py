56dddc747d683d3452e1821164f43cd10b61495c
EmilSantos <Santos.N.Emil@gmail.com>
2021-12-16 11:48:36 -0800

    Created validator template
    created validator and tested that it's working within shell.

--- DjangoPlayground/products/models.py
+++ DjangoPlayground/products/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from .validators import validate_blocked_words
+
 # Create your models here.
 class Product(models.Model):
-  title = models.CharField(max_length=120)
+  title = models.CharField(max_length=120, validators=[validate_blocked_words])
   description = models.TextField(null=True)
   price = models.DecimalField(max_digits=20, decimal_places=2)
---
+++ DjangoPlayground/products/validators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+
+BLOCKED_WORDS = ['CHEAP', 'BAD']
+
+def validate_blocked_words(value):
+  init_value = f"{value}".lower()
+  init_items = set(init_value.split())
+  blocked = set([x.lower() for x in BLOCKED_WORDS])
+  invalid_items = list(init_items & blocked)
+  has_error = len(invalid_items) > 0
+  if has_error:
+    validations_errors = []
+    for i, invalid in enumerate(invalid_items):
+      validations_errors.append(ValidationError("%(value)s is a blocked word",
+      params={'value': invalid}, code=f'blocked-word-{i}'))
+    raise ValidationError(validations_errors)
+  return value

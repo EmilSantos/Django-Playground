7618af31f0333912b8a68b27b7cb81321e633ac1
EmilSantos <Santos.N.Emil@gmail.com>
2021-12-16 13:20:21 -0800

    Model Field Validation modifications
    Overriding Save() and implementing Clean() method. implemented Choices.

--- DjangoPlayground/products/models.py
+++ DjangoPlayground/products/models.py
@@ -2,8 +2,35 @@ from django.db import models
 
 from .validators import validate_blocked_words
 
+# (DB_VALUE, USER_FACING_VALUE)
+PUBLIC_STATE_CHOICES = [
+  ('DR', 'Draft'),
+  ('PU', 'Publish'),
+  ('PR', 'Private'),
+]
+
 # Create your models here.
 class Product(models.Model):
   title = models.CharField(max_length=120, validators=[validate_blocked_words])
+  state = models.CharField(max_length=2, default="DR",
+  choices=PUBLIC_STATE_CHOICES)
   description = models.TextField(null=True)
   price = models.DecimalField(max_digits=20, decimal_places=2)
+
+  def save(self, *args, **kwargs):
+    validate_blocked_words(self.title)
+    super().save(*args, **kwargs)
+  
+  @property
+  def is_published(self):
+    return self.state == "PU"
+
+  """
+  def clean(self):
+    '''
+    Django Model Forms / Django Forms
+    Project.objects.create() -> not call .clean()
+    '''
+    if self.title == self.description:
+      raise ValidationError("Make the description different")
+  """
